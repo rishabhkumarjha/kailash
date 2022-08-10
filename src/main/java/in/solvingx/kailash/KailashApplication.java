f9a9425fa12a79d4e9088943d8b587fdb394aac4
Rishabh Jha <rishabhkumarjha@gmail.com>
2022-08-10 18:29:14 +0530

    Initial commit

---
+++ src/main/java/in/solvingx/kailash/KailashApplication.java
@@ -0,0 +1,13 @@
+package in.solvingx.kailash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KailashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KailashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/solvingx/kailash/KailashApplicationTests.java
@@ -0,0 +1,13 @@
+package in.solvingx.kailash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KailashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
